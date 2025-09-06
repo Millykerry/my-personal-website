266eaca24293a217f3d5a7acf0c718e2c8134443
millykerry <kerryom975@gmail.com>
2025-09-06 23:07:28 +0300

    Move script.js into js folder and update index.html

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//script.js
+const h2 = document.createElement("h2")
+h2.txtContent = "This content added by JavaScript";
+
+// add the <h2> into the body
+document.queryselectio("body").appendChild(h2)
